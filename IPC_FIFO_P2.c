2accc48c2be8bac0eb04487f521f24e182e44ea9
akagarw <47278895+akagarw@users.noreply.github.com>
2020-11-02 13:26:08 +0530

    IPC using Named Pipes/FIFOs

---
+++ IPC_FIFO_P2.c
@@ -0,0 +1,49 @@
+// C program to implement one side of FIFO 
+// This side writes first, then reads 
+#include <stdio.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main() 
+{ 
+	int fd; 
+	char readBuffer[100];
+	char userInput[100];
+	char *fifoPipe = "/tmp/fifoFile"; 
+	
+	mkfifo(fifoPipe,0666); 
+
+	printf("\n  Process 2 running... Enter \"end\" to end the IPC...\n");
+	while (1) 
+	{ 
+		fd = open(fifoPipe, O_RDONLY); 
+		read(fd,readBuffer,sizeof(readBuffer)); 
+		close(fd);
+		if(strcmp(readBuffer,"end")==0)
+		{
+			printf("\n  IPC ended by other Process...\n\n");
+			break;
+		}
+		printf("\n\tMessage Received from Process 1: %s", readBuffer); 
+		 
+		
+
+		fd = open(fifoPipe, O_WRONLY); 
+		printf("\n\tEnter Message : ");
+		fgets(userInput, 100, stdin); 
+		printf("\t  Sending Message...");
+		userInput[strlen(userInput)-1]='\0';
+		write(fd, userInput,strlen(userInput)+1); 
+		close(fd); 
+		if(strcmp(userInput,"end")==0)
+		{
+			printf("\n  Ending IPC...\n\n");
+			break;
+		}
+
+	} 
+	return 0; 
+} 
